a2bfe5934fe1bee3c790a405677927b3e05f6034
Riteshsingh-D15 <cosmo1rit@gmail.com>
2024-11-08 22:26:55 +0530

    This is the full file of the project (cse326)
    Date of upload(7/11/2024)

---
+++ project/java.js
@@ -0,0 +1,24 @@
+let search = document.querySelector('.search-box');
+let menu = document.querySelector('.navbar');
+
+document.querySelector('#search-icon').onclick = () => {
+    search.classList.toggle('active');
+    menu.classList.remove('active');
+}
+
+document.querySelector('#menu-icon').onclick = () => {
+    menu.classList.toggle('active');
+    search.classList.remove('active');
+}
+
+// Hide menu and search box on scroll
+window.onscroll = () => {
+    menu.classList.remove('active');
+    search.classList.remove('active');
+}
+
+// Header shadow effect
+let header = document.querySelector('header');
+window.addEventListener('scroll', () => {
+    header.classList.toggle('shadow', window.scrollY > 0);
+});
